d1cf4caf4ca8bffc888d75f0ef168f6611504ce5
Ivan Dario <darivanarsa@gmail.com>
2024-08-11 12:18:00 -0500

    menu

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const menu = document.querySelector(".menu")
+
+const nav = document.querySelector(".menu_desplegable")
+
+const iconWasa = document.querySelector(".fixed-span")
+
+menu.addEventListener("click", (event) => {
+event.preventDefault()
+
+if (window.innerWidth <= 893) {
+if (nav.style.display === "none" || nav.style.display === "") {
+    nav.style.display = "block";
+    menu.style.backgroundImage = 'url("./img/x.png")';
+    iconWasa.style.opacity = "0.5";
+  } else {
+    nav.style.display = "none";
+    menu.style.backgroundImage = 'url("./img/menu.svg")';
+    iconWasa.style.opacity = "100%";
+  }
+}
+});
+
+nav.addEventListener ("click", () =>{
+  if (window.innerWidth <= 893) {
+  nav.style.display = "none";
+  menu.style.backgroundImage = 'url("./img/menu.svg")';
+  iconWasa.style.opacity = "100%";
+  }
+})
+
+
+
 document.querySelectorAll('.tooltip').forEach(span => {
     span.addEventListener('mouseover', function(event) {
       const tooltip = document.getElementById('tooltip');
